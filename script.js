e3d54ebe7e8b92961b37cd74a00a0fb5c73896ca
Liz Krane <learningnerd@gmail.com>
2018-04-09 19:38:28 -0700

    Create db on button click, wrap in helper function
      - Wrap database creation code in helper function createDatabase(dbName, version)
      - Create database on button click

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log('JavaScript file loaded :) Testing IndexedDB database setup:');
 // https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
 
 const DB_NAME = "setup-test";
-const DB_VERSION = 1;
+const DB_VERSION = 2;
 
 // IIFE!!! If no browser support, don't run any of the code in this function
 (function() {
@@ -17,17 +17,27 @@ const DB_VERSION = 1;
     return;
   }
 
+  // ON BUTTON CLICK: CREATE DATABASE:
+  document.getElementById("create-database").addEventListener("click", handleClickCreateDatabase);
+    function handleClickCreateDatabase (event) {
   
+      createDatabase(DB_NAME, DB_VERSION);
+      
+    } // END: handleClickCreateDatabase
+
+})(); // End of IIFE
+
+function createDatabase(dbName, version) {
   // FIRST, OPEN A DATABASE -- reference:
-  // https://developer.mozilla.org/en-US/docs/Web/API/IDBFactory/open
+    // https://developer.mozilla.org/en-US/docs/Web/API/IDBFactory/open
       // indexedDB.open(name);
       // indexedDB.open(name, version);
           // RETURNS: A IDBOpenDBRequest object on which subsequent events related to this request are fired.
           // https://developer.mozilla.org/en-US/docs/Web/API/IDBOpenDBRequest
-  var openDbRequest = window.indexedDB.open(DB_NAME, DB_VERSION);    
+  var openDbRequest = window.indexedDB.open(dbName, version);    
   console.log("Created openDbRequest:");
   console.log(openDbRequest);
-  
+
   // HANDLE UPGRADE NEEDED EVENT -- triggered by open() first time it's run, or if version number changes
     // https://developer.mozilla.org/en-US/docs/Web/API/IDBOpenDBRequest/onupgradeneeded
     // https://developer.mozilla.org/en-US/docs/Web/API/IDBVersionChangeEvent
@@ -35,12 +45,8 @@ const DB_VERSION = 1;
     function handleUpgradeDb (event) {
       console.log("'upgradeneeded' event triggered! Event: ");
       console.log(event);
-      
-      console.log("db: ");
-      var db = openDbRequest.result;
-      console.log(db);
     }
-  
+
   // HANDLE SUCCESS EVENT (triggered after "upgradeneeded"):
     // https://developer.mozilla.org/en-US/docs/Web/API/IDBFactory/open
     // https://developer.mozilla.org/en-US/docs/Web/API/IDBRequest
@@ -48,10 +54,12 @@ const DB_VERSION = 1;
     function handleOpenDbSuccess (event) {
       console.log("Open DB request: success! Event: ");
       console.log(event);
-      
+
       var db = openDbRequest.result;
+      console.log("db:");      
+      console.log(db);
     }
-  
+
   // HANDLE ERROR EVENT
     // https://developer.mozilla.org/en-US/docs/Web/API/IDBRequest/error
     // https://developer.mozilla.org/en-US/docs/Web/API/IDBFactory/open
@@ -62,5 +70,4 @@ const DB_VERSION = 1;
       console.log("Event object: ");
       console.log(event);
     }
-
-})(); // End of IIFE
+} // END: createDatabase
